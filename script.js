1f47599cc54e61a3294d483e3f7a4eb2986e4744
Lynn Ventura <m_lynnventura@yahoo.com>
2023-01-09 20:25:46 -0800

    removed script tag from HTML & created JS file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.getElementById("button").onclick = function() {
+    setBackgroundColorById("paragraph", "blue");
+}
+document.getElementById("alert").onclick = function(){
+    alert(document.getElementById("popup-input").value);
+}
+document.getElementById("hover-this").onmouseover = function(){
+    setBackgroundColorById("body", "red");
+}
+document.getElementById("hover-this").onmouseout = function(){
+    setBackgroundColorById("body", "white");
+}
+function getValueFromId(id){
+    return document.getElementById(id).value;
+}
+function setBackgroundColorById(id, color){
+    document.getElementById(id).style = "background-color: " + color;
+}
+function mouseOverFunction(el){
+    el.style = "background-color: black";
+}
