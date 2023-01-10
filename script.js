6b1b16709c5a8db7d8e8bee81a65f33808ed6fbf
Lynn Ventura <m_lynnventura@yahoo.com>
2023-01-09 20:46:31 -0800

    changed functions into arrow functions

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
+document.getElementById("button").onclick = () => setBackgroundColorById("paragraph", "blue");
 
-document.getElementById("button").onclick = function() {
-    setBackgroundColorById("paragraph", "blue");
-}
-document.getElementById("alert").onclick = function(){
-    alert(document.getElementById("popup-input").value);
-}
-document.getElementById("hover-this").onmouseover = function(){
-    setBackgroundColorById("body", "red");
-}
-document.getElementById("hover-this").onmouseout = function(){
-    setBackgroundColorById("body", "white");
-}
-function getValueFromId(id){
-    return document.getElementById(id).value;
-}
-function setBackgroundColorById(id, color){
-    document.getElementById(id).style = "background-color: " + color;
-}
-function mouseOverFunction(el){
-    el.style = "background-color: black";
-}
+document.getElementById("alert").onclick = () => alert(document.getElementById("popup-input").value);
+
+document.getElementById("hover-this").onmouseover = () => setBackgroundColorById("body", "red");
+
+document.getElementById("hover-this").onmouseout = () => setBackgroundColorById("body", "white");
+
+const getValueFromId = (id) => document.getElementById(id).value;
+
+const setBackgroundColorById = (id, color) => document.getElementById(id).style = "background-color: pink" + color;
+
+const mouseOverFunction = (el) => el.style = "background-color: black";
